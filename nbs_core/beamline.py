4fa2b56e0b4fd88e26a7ac5a82dc10f9e1ea6357
Charles Titus <ctitus@bnl.gov>
2024-09-08 23:13:15 -0400

    Tweaks to building basic beamline objects in a more automatic way

--- nbs_core/beamline.py
+++ nbs_core/beamline.py
@@ -1,33 +1,42 @@
-from .autoload import loadFromConfig
-
-
 class BeamlineModel:
-    def __init__(self, devices, groups, roles,  *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    default_groups = [
+        "shutters",
+        "gatevalves",
+        "apertures",
+        "pinholes",
+        "gauges",
+        "motors",
+        "detectors",
+        "manipulators",
+        "mirrors",
+        "controllers",
+        "vacuum",
+        "misc",
+    ]
+
+    def __init__(self, devices, groups, roles, *args, **kwargs):
+        super().__init__(
+            *args, **kwargs
+        )  # Needed for multiple inheritance! Do not remove!
         self.devices = devices
-        self.shutters = {}
-        self.gatevalves = {}
-        self.apertures = {}
-        self.pinholes = {}
-        self.gauges = {}
-        self.motors = {}
-        self.detectors = {}
-        self.manipulators = {}
-        self.mirrors = {}
-        self.controllers = {}
-        self.vacuum = {}
         self.energy = None
         self.primary_manipulator = None
         self.default_shutter = None
-        self.groups = ["shutters", "gatevalves", "apertures", "pinholes", "gauges", "motors", "detectors", "manipulators", "mirrors", "controllers", "vacuum", "misc"]
+        self.groups = list(self.default_groups)  # Create a copy of the default groups
         self.roles = ["energy", "primary_manipulator", "default_shutter"]
+
+        # Initialize empty dictionaries for each default group
+        for group in self.default_groups:
+            setattr(self, group, {})
+
         self.loadDevices(devices, groups, roles)
 
     def loadDevices(self, devices, groups, roles):
         self.devices.update(devices)
         for groupname, devicelist in groups.items():
-            self.groups.append(groupname)
-            setattr(self, groupname, {})
+            if groupname not in self.groups:
+                self.groups.append(groupname)
+                setattr(self, groupname, {})
             gdict = getattr(self, groupname)
             for key in devicelist:
                 print(f"Setting {groupname}[{key}]")
